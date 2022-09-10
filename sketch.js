c8f73c11e98d5c0a4b0a540ef76acb0b6ff4e81b
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-09-10 13:59:53 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -859,6 +859,7 @@ function handlePotencia(){
     console.log("Potencia:"+potencianumbers);
     result = pow(result, potencianumbers);
     console.log(result);
+    dot = true;
 
     potencianumbers = "";
   }
